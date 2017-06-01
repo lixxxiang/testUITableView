32b6f1961ff5d094115c5d0c5dc835d5d132f203
lixxxiang <13051744716@163.com>
2017-06-01 11:08:37 +0800

    dfd

--- testTableView/ViewController.m
+++ testTableView/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+NSArray *array;
 @interface ViewController ()
 
 @end
@@ -17,8 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    array = [NSArray arrayWithObjects:@"a", @"b", @"c", nil];
+    _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
 }
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString* cellId = @"cellId";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellId];
+    NSUInteger index = indexPath.row;
+
+    cell.textLabel.text = [array objectAtIndex:index];
+    cell.detailTextLabel.text = [array objectAtIndex:index];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return array.count;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
