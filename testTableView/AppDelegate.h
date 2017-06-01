53a7b1bbf4f80338db540c5404315bdb4d73a4a9
lixxxiang <13051744716@163.com>
2017-06-01 09:36:31 +0800

    Initial Commit

---
+++ testTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testTableView
+//
+//  Created by yyfwptz on 2017/6/1.
+//  Copyright © 2017年 yyfwptz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testTableView
+//
+//  Created by yyfwptz on 2017/6/1.
+//  Copyright © 2017年 yyfwptz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
